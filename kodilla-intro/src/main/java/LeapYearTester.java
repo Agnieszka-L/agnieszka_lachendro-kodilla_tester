eb23efaa35e1f1e1d8838afee2886014f723aa2f
Agnieszka Lachendro <a.lachendro@gmail.com>
2020-01-27 21:13:34 -0600

    Moved LeapYear.java and LeapYearTester.java to kodilla-into

--- kodilla-intro/src/main/java/LeapYearTester.java
+++ kodilla-intro/src/main/java/LeapYearTester.java
@@ -4,12 +4,12 @@ public class LeapYearTester {
         boolean leap2040 = year.isLeap(2040);
         boolean leap100 = year.isLeap(100);
         boolean leap2000 = year.isLeap(2000);
-        boolean leap400  = year.isLeap(400);
+        boolean leap400 = year.isLeap(400);
 
 
         System.out.println("leap2040 = " + leap2040);
         System.out.println("leap100  = " + leap100);
         System.out.println("leap2000  = " + leap2000);
         System.out.println("leap400  = " + leap400);
-    20}
+    }
 }
